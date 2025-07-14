cf10d525cbff8b62f8f14ec2f4e27ea67951ff2f
abhinav565 <abhinav65vrma@gmail.com>
2025-07-14 20:56:20 +0530

    config

--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,7 @@ const prisma = new PrismaClient();
 
 // CORS configuration
 app.use(cors({
-  origin: '*', // Vite dev server
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
