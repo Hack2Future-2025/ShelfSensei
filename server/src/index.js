b0327b3e196ebc9fc122e9f739580db46203bb21
abhinav565 <abhinav65vrma@gmail.com>
2025-07-14 20:59:21 +0530

    config

--- server/src/index.js
+++ server/src/index.js
@@ -23,7 +23,6 @@ app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
 }));
 
 app.use(express.json());
